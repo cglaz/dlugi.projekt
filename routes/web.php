3f496ac542379647287b73ddac2b335188331fd4
cezazzr <cezazzr@gmail.com>
2021-08-01 22:36:08 +0200

    Quick fix

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',['middleware' => 'guest', function () {
+Route::get('/', function () {
     return view('auth.login');
-}]);
+})->middleware('guest');
 
 Auth::routes();
 
